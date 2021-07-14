b1e74a3fa7483129ee97a40b091305e60953bdda
bego <begomontes7@gmail.com>
2021-07-14 13:59:41 -0500

    Routes for API

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express= require('express');
+
+const app = express();
+
+const fetch   = require('node-fetch');
+
+
+cors = require("cors");
+app.use(cors());
+
+
+// general route
+app.get('/', (req,res)=>{
+    let url ="https://itunes.apple.com/search?term=adele&entity=album"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        res.status(200).send({ data });
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+
+// route to get all albums for a given artist
+app.get('/api/albums/:artist', (req,res)=>{
+    let url = `https://itunes.apple.com/search?term=${req.params.artist}&entity=album`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        res.send({ data });
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+
+
+
+const port =process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+});
---
+++ backend/tests/albums.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const expect = require('chai').expect
+const request = require('supertest');
+const app = require('../index');
+
+describe('Unit testing the / route', function() {
+
+    it('should return OK status', function() {
+      return request(app)
+        .get('/')
+        .then(function(response){
+            assert.equal(response.status, 200)
+        })
+    });
+});
